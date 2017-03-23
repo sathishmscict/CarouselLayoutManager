7f0c4a82e40d5ef57b13394e03f9a48e878ff31e
Saul Molinero <saulmm2@gmail.com>
2017-03-23 10:57:47 +0100

    Modified sample to fit cui2 usecase

--- CarouselLayoutManager/sample/src/main/java/com/azoft/carousellayoutmanager/sample/CarouselPreviewActivity.java
+++ CarouselLayoutManager/sample/src/main/java/com/azoft/carousellayoutmanager/sample/CarouselPreviewActivity.java
@@ -27,47 +27,14 @@ public class CarouselPreviewActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ActivityCarouselPreviewBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_carousel_preview);
-
-        setSupportActionBar(binding.toolbar);
+        final ActivityCarouselPreviewBinding binding = DataBindingUtil
+            .setContentView(this, R.layout.activity_carousel_preview);
 
         final TestAdapter adapter = new TestAdapter();
 
         // create layout manager with needed params: vertical, cycle
-        initRecyclerView(binding.listHorizontal, new CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL, false), adapter);
-        initRecyclerView(binding.listVertical, new CarouselLayoutManager(CarouselLayoutManager.VERTICAL, true), adapter);
-
-        // fab button will add element to the end of the list
-        binding.fabScroll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-/*
-                final int itemToRemove = adapter.mItemsCount;
-                if (10 != itemToRemove) {
-                    adapter.mItemsCount++;
-                    adapter.notifyItemInserted(itemToRemove);
-                }
-*/
-                binding.listHorizontal.smoothScrollToPosition(adapter.getItemCount() - 2);
-                binding.listVertical.smoothScrollToPosition(adapter.getItemCount() - 2);
-            }
-        });
-
-        // fab button will remove element from the end of the list
-        binding.fabChangeData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-/*
-                final int itemToRemove = adapter.mItemsCount - 1;
-                if (0 <= itemToRemove) {
-                    adapter.mItemsCount--;
-                    adapter.notifyItemRemoved(itemToRemove);
-                }
-*/
-                binding.listHorizontal.smoothScrollToPosition(1);
-                binding.listVertical.smoothScrollToPosition(1);
-            }
-        });
+        initRecyclerView(binding.listVertical, new CarouselLayoutManager(
+            CarouselLayoutManager.VERTICAL, true), adapter);
     }
 
     private void initRecyclerView(final RecyclerView recyclerView, final CarouselLayoutManager layoutManager, final TestAdapter adapter) {
@@ -83,44 +50,18 @@ public class CarouselPreviewActivity extends AppCompatActivity {
         // enable center post scrolling
         recyclerView.addOnScrollListener(new CenterScrollListener());
         // enable center post touching on item and item click listener
-        DefaultChildSelectionListener.initCenterItemListener(new DefaultChildSelectionListener.OnCenterItemClickListener() {
-            @Override
-            public void onCenterItemClicked(@NonNull final RecyclerView recyclerView, @NonNull final CarouselLayoutManager carouselLayoutManager, @NonNull final View v) {
-                final int position = recyclerView.getChildLayoutPosition(v);
-                final String msg = String.format(Locale.US, "Item %1$d was clicked", position);
-                Toast.makeText(CarouselPreviewActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-        }, recyclerView, layoutManager);
-
-        layoutManager.addOnItemSelectionListener(new CarouselLayoutManager.OnCenterItemSelectionListener() {
-
-            @Override
-            public void onCenterItemChanged(final int adapterPosition) {
-                if (CarouselLayoutManager.INVALID_POSITION != adapterPosition) {
-                    final int value = adapter.mPosition[adapterPosition];
-/*
-                    adapter.mPosition[adapterPosition] = (value % 10) + (value / 10 + 1) * 10;
-                    adapter.notifyItemChanged(adapterPosition);
-*/
-                }
-            }
-        });
     }
 
     private static final class TestAdapter extends RecyclerView.Adapter<TestViewHolder> {
 
         @SuppressWarnings("UnsecureRandomNumberGeneration")
-        private final Random mRandom = new Random();
-        private final int[] mColors;
         private final int[] mPosition;
-        private int mItemsCount = 10;
+        private int mItemsCount = 5;
 
         TestAdapter() {
-            mColors = new int[mItemsCount];
             mPosition = new int[mItemsCount];
             for (int i = 0; mItemsCount > i; ++i) {
                 //noinspection MagicNumber
-                mColors[i] = Color.argb(255, mRandom.nextInt(256), mRandom.nextInt(256), mRandom.nextInt(256));
                 mPosition[i] = i;
             }
         }
@@ -132,9 +73,7 @@ public class CarouselPreviewActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(final TestViewHolder holder, final int position) {
-            holder.mItemViewBinding.cItem1.setText(String.valueOf(mPosition[position]));
-            holder.mItemViewBinding.cItem2.setText(String.valueOf(mPosition[position]));
-            holder.itemView.setBackgroundColor(mColors[position]);
+            holder.bind();
         }
 
         @Override
@@ -151,6 +90,10 @@ public class CarouselPreviewActivity extends AppCompatActivity {
             super(itemViewBinding.getRoot());
 
             mItemViewBinding = itemViewBinding;
+        }
+
+        public void bind() {
+            mItemViewBinding.text.setText("Item: "+getAdapterPosition());
         }
     }
 }
